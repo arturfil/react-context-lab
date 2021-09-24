0e4053f7cbfc548e120b8325713f58b7e4b7d168
Andriw Tapanes <andriwtapanes@gmail.com>
2021-09-24 15:07:24 -0400

    Finished iterations

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { useContext } from "react";
+import { FoodContext } from "./context/FoodContext";
+import useForm from "./hooks/useForm";
 
 function App() {
+  const { foods, setFoods, addFood } = useContext(FoodContext);
+
+  const { foodName, foodType, protein, calories, handleChange, resetForm } =
+    useForm({
+      foodName: "",
+      foodType: "",
+      protein: "",
+      calories: "",
+    });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newFood = {
+      foodName,
+      foodType,
+      protein,
+      calories,
+    };
+    setFoods([newFood, ...foods]);
+    resetForm();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Damn you FAT</h2>
+      <p>Diet tracking App</p>
+      <form>
+        <input
+          onChange={handleChange}
+          placeholder="Food Name"
+          value={foodName}
+          className="form-control"
+          type="text"
+          name="foodName"
+        />
+        <input
+          onChange={handleChange}
+          placeholder="Food Type"
+          value={foodType}
+          className="form-control"
+          type="text"
+          name="foodType"
+        />
+        <input
+          onChange={handleChange}
+          placeholder="Protein Content"
+          value={protein}
+          className="form-control"
+          type="text"
+          name="protein"
+        />
+        <input
+          onChange={handleChange}
+          placeholder="Calories Content"
+          value={calories}
+          className="form-control"
+          type="text"
+          name="calories"
+        />
+        <br />
+        <button onClick={handleSubmit}>Submit this shit</button>
+        <div>
+          {foods &&
+            foods.map((food, index) => <div key={index}>{food.foodName}</div>)}
+        </div>
+      </form>
     </div>
   );
 }
---
+++ src/hooks/useForm.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+const useForm = (object) => {
+  const [state, setState] = useState(object);
+
+  const handleChange = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const resetForm = () => {
+    setState(object);
+  };
+
+  return {
+    ...state,
+    handleChange,
+    resetForm,
+  };
+};
+
+export default useForm;
