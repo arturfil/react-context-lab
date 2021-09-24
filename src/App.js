5ed3a20b786265109a0df6a3d1b42af8032a981e
Alvin Sewram <alvinsewram@Alvins-MacBook-Pro.local>
2021-09-24 15:06:29 -0400

    finished lab

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useContext } from "react";
+import { FoodContext } from "./FoodContext";
+import useForm from "./useForm";
+
+const object = {
+  foodName: "",
+  foodType: "",
+  protein: "",
+  calories: "",
+};
 
 function App() {
+  const { foodItems, setFoodItems } = useContext(FoodContext);
+  const { foodName, foodType, protein, calories, handleChange, resetForm } =
+    useForm(object);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const newFood = {
+      foodName: foodName,
+      foodType: foodType,
+      protein: protein,
+      calories: calories,
+    };
+
+    setFoodItems([newFood, ...foodItems]);
+    resetForm();
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <input
+          name="foodName"
+          type="text"
+          placeholder="Enter Food Name"
+          value={foodName}
+          onChange={handleChange}
+        />
+        <input
+          name="foodType"
+          type="text"
+          placeholder="Enter Food Type"
+          value={foodType}
+          onChange={handleChange}
+        />
+        <input
+          name="protein"
+          type="text"
+          placeholder="Enter Protein"
+          value={protein}
+          onChange={handleChange}
+        />
+        <input
+          name="calories"
+          type="text"
+          placeholder="Enter Calories"
+          value={calories}
+          onChange={handleChange}
+        />
+        <button onClick={onSubmit}>submit</button>
+      </form>
+
+      <div>
+        {foodItems.map((foodItem, idx) => {
+          return <div key={idx}> {foodItem.foodName} </div>;
+        })}
+      </div>
     </div>
   );
 }
